ff274557bd9acd265f207747445a8c6489eea933
Jeremy Morgan <jeremymorgan@gmail.com>
2015-05-13 22:12:54 -0700

    initial commit

---
+++ XMLWorks.Library/XMLWorker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLWorks.Library
+{
+    public class XMLWorker
+    {
+        public string XMLFileName { get; set; }
+
+        public void ReadXMLFile()
+        {
+            try
+            {
+                XElement xelement = XElement.Load(XMLFileName);
+                IEnumerable<XElement> employees = xelement.Elements();
+
+                // Read the entire XML
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee);
+                }
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+    }
+}
---
+++ XMLWorks.Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XMLWorks.Library;
+
+namespace XMLWorks.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLWorker ourXMLWorker = new XMLWorker();
+
+            ourXMLWorker.XMLFileName = "Employees.xml";
+            ourXMLWorker.ReadXMLFile();
+
+            Console.ReadLine();
+
+        }
+    }
+}
